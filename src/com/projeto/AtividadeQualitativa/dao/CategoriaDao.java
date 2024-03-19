dcefcf710940010b0ac023a8f6e4c9a4b199a7fa
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2024-03-19 18:17:22 -0300

    Cria crud para categoria

--- src/com/projeto/AtividadeQualitativa/dao/CategoriaDao.java
+++ src/com/projeto/AtividadeQualitativa/dao/CategoriaDao.java
@@ -1,6 +1,8 @@
 package com.projeto.AtividadeQualitativa.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -16,22 +18,110 @@ public class CategoriaDao {
     }
 
     public Categoria criaCategoria(Categoria categoria) throws SQLException {
-        return categoria;
+
+        String sql = "insert into Categoria (nome, descricao) values (?,?)";
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, categoria.getNome());
+            statement.setString(2, categoria.getDescricao());
+            statement.execute();
+            statement.close();
+            return categoria;
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     public Categoria buscaCategoriaPorId(Long id) throws SQLException {
-        return null;
+        String sql = "select * from Categoria where id=?";
+
+        Categoria categoria = new Categoria();
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setLong(1, id);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()){
+                categoria.setId(result.getLong("id"));
+                categoria.setNome(result.getString("nome"));
+                categoria.setDescricao(result.getString("descricao"));
+            }
+
+            result.close();
+            statement.close();
+
+            return categoria;
+        }
+        catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     public ArrayList<Categoria> buscaCategoria() throws SQLException {
-        return null;
+
+        String sql = "Select * from Categoria";
+
+        ArrayList<Categoria> categorias = new ArrayList<Categoria>();
+
+        try{
+            PreparedStatement statement = this.connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                Categoria categoria = new Categoria();
+                categoria.setId(result.getLong("id"));
+                categoria.setNome(result.getString("nome"));
+                categoria.setDescricao(result.getString("descricao"));
+                categorias.add(categoria);
+            }
+            result.close();
+            statement.close();
+            return categorias;
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     public void deletaCategoria(Long id) throws SQLException {
 
+        String sql = "Delete from Categoria where id=?";
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setLong(1, id);
+            statement.execute();
+            statement.close();
+        }
+        catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+
     }
 
-    public Categoria atualizCategoria(Categoria categoria) throws SQLException {
-        return categoria;
+    public Categoria atualizaCategoria(Categoria categoria) throws SQLException {
+
+        String sql = "Update Categoria set nome=?, descricao=? where id=?";
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setLong(1, categoria.getId());
+            statement.setString(2, categoria.getNome());
+            statement.setString(3, categoria.getDescricao());
+
+            statement.execute();
+            statement.close();
+            return categoria;
+        }
+        catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
